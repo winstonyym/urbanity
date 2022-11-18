6d8cf90ee1e3966063bc19fcb5d6151801b8d926
winstonyym <winstonyym@u.nus.edu>
2022-11-18 16:28:41 +0800

    build: update python version

--- src/urbanity/utils.py
+++ src/urbanity/utils.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import os
 import json
 import pkg_resources
-from typing import Optional
 from IPython.display import display
 from ipyleaflet import DrawControl
 
